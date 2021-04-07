e03b448da8d0f1a9eecf88bc0d5819bf04357c71
Burak Doğan <bdogan85@gmail.com>
2021-04-07 17:58:29 +0300

    Initial Commit

---
+++ constant/units.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * UBL-TR 1.2 için varsayılan birim kodları
+ * Burak Doğan <me@burak-dogan.com>
+ */
+
+return [
+    "B32" => "KG-METRE KARE",
+    "C62" => "ADET(UNIT)",
+    "CCT" => "TON BAŞINA TAŞIMA KAPASİTESİ",
+    "PR" => "ÇİFT",
+    "D30" => "BRÜT KALORİ DEĞERİ D40 BİN LİTRE",
+    "GFI" => "FISSILE İZOTOP GRAMI GRM GRAM",
+    "GT" => "GROSS TON",
+    "CEN" => "YÜZ ADET",
+    "KPO" => "KİLOGRAM POTASYUM OKSİT",
+    "MND" => "KURUTULMUŞ NET AĞIRLIKLI KİLOGRAMI 3I KİLOGRAM-ADET",
+    "KFO" => "DİFOSFOR PENTAOKSİT KİLOGRAMI KGM KİLOGRAM",
+    "KHY" => "HİDROJEN PEROKSİT KİLOGRAMI KMA METİL AMİNLERİN KİLOGRAMI",
+    "KNI" => "AZOTUN KİLOGRAMI",
+    "KPH" => "KİLOGRAM POTASYUM HİDROKSİT KSD %90 KURU ÜRÜN KİLOGRAMI",
+    "KSH" => "SODYUM HİDROKSİT KİLOGRAMI KUR URANYUM KİLOGRAMI",
+    "D32" => "TERAWATT SAAT",
+    "GWH" => "GİGAWATT SAAT",
+    "MWH" => "MEGAWATT SAAT (1000 kW.h)",
+    "KWH" => "KİLOWATT SAAT KWT KİLOWATT",
+    "LPA" => "SAF ALKOL LİTRESİ",
+    "LTR" => "LİTRE",
+    "MTK" => "METRE KARE DMK DESİMETRE KARE",
+    "MTQ" => "METRE KÜP",
+    "MTR" => "METRE",
+    "NCL" => "HÜCRE ADEDİ",
+    "CTM" => "KARAT",
+    "SM3" => "STANDART METREKÜP",
+    "R9" => "BİN METRE KÜP SET SET",
+    "T3" => "BİN ADET",
+];
